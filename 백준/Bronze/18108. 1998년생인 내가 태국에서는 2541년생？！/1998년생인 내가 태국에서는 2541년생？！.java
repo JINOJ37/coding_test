be6c449e8e7c66151c570a9e74ae83620a917d04
JINOJ37 <127945095+JINOJ37@users.noreply.github.com>
2026-04-05 12:29:52 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 108 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int year = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(year-543);
+
+        System.out.print(sb);
+    }
+}
