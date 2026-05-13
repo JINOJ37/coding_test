ac9b84ec7b99148ee5c7dfcb3efe418a37d1e078
JINOJ37 <127945095+JINOJ37@users.noreply.github.com>
2026-05-13 11:38:14 +0900

    [level 0] Title: 글자 지우기, Time: 0.12 ms, Memory: 60.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        StringBuilder answer = new StringBuilder();
+        
+        boolean[] isDeleted = new boolean[my_string.length()];
+        
+        for(int index : indices){
+            isDeleted[index] = true;
+        }
+        
+        for(int i=0; i<my_string.length(); i++){
+            if(!isDeleted[i]){
+                answer.append(my_string.charAt(i));
+            }
+        }
+        return answer.toString();
+    }
+}
