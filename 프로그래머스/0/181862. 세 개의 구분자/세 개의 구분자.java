fc3b0e5ee61624994e75c6490418b086b8ea376b
JINOJ37 <127945095+JINOJ37@users.noreply.github.com>
2026-05-29 15:44:50 +0900

    [level 0] Title: 세 개의 구분자, Time: 27.41 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class Solution {
+    public String[] solution(String myStr) {
+        String[] answer = Arrays.stream(myStr.split("[abc]+")).filter(i->!i.isEmpty()).toArray(String[]::new);
+        if(answer.length == 0)
+            return new String[]{"EMPTY"};
+        return answer;
+    }
+}
