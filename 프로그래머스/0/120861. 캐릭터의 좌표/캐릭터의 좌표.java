bc13dc6bcdd2a7e8d25a960fb380f4344e0b6b6e
JINOJ37 <127945095+JINOJ37@users.noreply.github.com>
2026-06-19 17:16:53 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = new int[]{0,0};
+        for(String key : keyinput){
+            switch(key){
+                case "left" : 
+                    if(answer[0]-1 >= -1 * (board[0]/2)) answer[0] --;
+                    break;
+                case "right" :
+                    if(answer[0]+1 <= board[0]/2) answer[0] ++;
+                    break;
+                case "up" : 
+                    if(answer[1]+1 <= board[1]/2) answer[1] ++;
+                    break;
+                case "down" : 
+                    if(answer[1]-1 >= -1 * (board[1]/2)) answer[1] --;
+                    break;
+            }
+        }
+        return answer;
+    }
+}
