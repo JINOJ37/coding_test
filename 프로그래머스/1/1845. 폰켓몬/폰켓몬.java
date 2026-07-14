ccfed873632ab513d3f1abf38520d1b9b2b3784c
JINOJ37 <127945095+JINOJ37@users.noreply.github.com>
2026-07-14 14:15:55 +0900

    [level 1] Title: 폰켓몬, Time: 10.23 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,15 +1,13 @@
+import java.util.Set;
 import java.util.HashSet;
-
 class Solution {
     public int solution(int[] nums) {
-        HashSet<Integer> map = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
         
         for(int num : nums){
-            map.add(num);
+            set.add(num);
         }
         
-        int result = (nums.length/2 < map.size()) ? nums.length/2 : map.size();
-        
-        return result;
+        return (set.size() < nums.length/2) ? set.size() : nums.length/2;
     }
 }
