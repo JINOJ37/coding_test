3a5c5671f9abc885cf3195cba91eb571e81ece7c
JINOJ37 <127945095+JINOJ37@users.noreply.github.com>
2026-07-21 09:45:24 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.52 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        Stack<Integer> bucket = new Stack<>();
+        HashMap<Integer, Stack<Integer>> map = new HashMap<>();
+        
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                if(board[board.length-i-1][j] != 0) map.computeIfAbsent(j+1, k -> new Stack<>()).push(board[board.length-i-1][j]);
+            }
+        }
+        
+        int answer = 0;
+        for(int m : moves){
+            if(map.get(m).empty()) continue;
+            
+            int doll = map.get(m).pop();
+            if(!bucket.empty() && doll == bucket.peek()) {
+                bucket.pop();
+                answer+=2;
+            } else{
+                bucket.push(doll);
+            }
+        }
+        
+        return answer;
+    }
+}
