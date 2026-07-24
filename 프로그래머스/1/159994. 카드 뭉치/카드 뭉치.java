a47fd1b3fdee00fbc310dd40cc32442f301d0247
JINOJ37 <127945095+JINOJ37@users.noreply.github.com>
2026-07-24 21:06:06 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,14 +1,6 @@
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        int i1 = 0, i2 = 0, i = 0;
-        while(i < goal.length){
-            if(i1 < cards1.length && goal[i].equals(cards1[i1])) {
-                i++; i1++;
-            } else if(i2 < cards2.length && goal[i].equals(cards2[i2])){
-                i++; i2++;
-            } else return "No";
-        }
-        
-        return "Yes";
+        String answer = "";
+        return answer;
     }
 }
