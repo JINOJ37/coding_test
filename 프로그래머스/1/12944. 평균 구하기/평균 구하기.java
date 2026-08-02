6632cf5d0e59a283e22b9fd703529f2984a421fe
baggytsk <127945095+baggytsk@users.noreply.github.com>
2026-08-02 23:35:23 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double solution(int[] arr) {
+        int answer = 0;
+        for(int n : arr){
+            answer += n;
+        }
+        return (double)answer/(double)arr.length;
+    }
+}
