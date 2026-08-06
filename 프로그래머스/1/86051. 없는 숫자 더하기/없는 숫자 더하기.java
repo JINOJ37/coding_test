5984dea83b5ec9f5bc51a9e78637b00f083e170e
baggytsk <127945095+baggytsk@users.noreply.github.com>
2026-08-06 23:23:42 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 0;
+        for(int n : numbers){
+            sum += n;
+        }
+        return 45-sum;
+    }
+}
